2dc2779669f1396109220f69634d293d2fe7cdc2
croak.g <croak.g@gmail.com>
2025-07-24 23:43:54 +0800

    Implement initial project structure and setup

---
+++ structs.go
@@ -0,0 +1,86 @@
+package tino
+
+import "time"
+
+type BaseResponse struct {
+	Status  bool   `json:"status"`
+	Message string `json:"message"`
+}
+
+// login structs
+type (
+	AuthRequest struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	AuthResponse struct {
+		BaseResponse
+		Data AuthData `json:"data"`
+	}
+	AuthData struct {
+		User  AuthUserData  `json:"user"`
+		Token AuthTokenData `json:"token"`
+	}
+	AuthUserData struct {
+		ID        string    `json:"id"`
+		Email     string    `json:"email"`
+		Profile   string    `json:"profile"`
+		Status    string    `json:"status"`
+		CreatedAt time.Time `json:"created_at"`
+	}
+	AuthTokenData struct {
+		Token           string    `json:"token"`
+		RefreshToken    string    `json:"refresh_token"`
+		RefreshExpireAt time.Time `json:"refresh_expires_at"`
+		ExpiresAt       time.Time `json:"expires_at"`
+	}
+)
+
+// invoice structs
+type (
+	InvoiceRequest struct {
+		WalletID      string `json:"wallet_id"` // Төлбөр хүлээн авах мерчантын хэтэвч
+		ExpiresMinute int    `json:"expires_minute"`
+		Description   string `json:"description"`
+		Amount        int    `json:"amount"`
+		TransactionID string `json:"transaction_id"` // third party id
+		CallbackURL   string `json:"callback_url"`
+		MetaData      string `json:"meta_data"`
+	}
+	InvoiceResponse struct {
+		BaseResponse
+		Data InvoiceData `json:"data"`
+	}
+	InvoiceData struct {
+		InvoiceID   string    `json:"invoice_id"`
+		MerchantID  string    `json:"merchant_id"`
+		WalletID    string    `json:"wallet_id"`
+		Amount      int       `json:"amount"`
+		Status      string    `json:"status"`
+		QrCode      string    `json:"qr_code"`
+		ExpiresAt   time.Time `json:"expires_at"`
+		CallbackURL string    `json:"callback_url"`
+		CreatedAt   time.Time `json:"created_at"`
+	}
+
+	InvoiceCheckResponse struct {
+		BaseResponse
+		Data InvoiceCheckData `json:"data"`
+	}
+	InvoiceCheckData struct {
+		InvoiceID string    `json:"invoice_id"`
+		Status    string    `json:"status"`
+		Amount    int       `json:"amount"`
+		PaidAt    time.Time `json:"paid_at"`
+	}
+	InvoiceCallbackResponse struct {
+		BaseResponse
+		Data InvoiceCallbackData `json:"data"`
+	}
+	InvoiceCallbackData struct {
+		InvoiceID string `json:"invoice_id"`
+		Status    string `json:"status"`
+		Amount    int    `json:"amount"`
+	}
+)
---
+++ tino.go
@@ -0,0 +1,128 @@
+package tino
+
+import (
+	"errors"
+	"time"
+
+	"resty.dev/v3"
+)
+
+type tino struct {
+	baserUrl string
+	username string
+	password string
+	client   *AuthData
+}
+
+type Tino interface {
+	CreateInvoice(invoice *InvoiceRequest) (*InvoiceResponse, error)
+	CancelInvoice(invoiceId string) (bool, error)
+	CheckInvoice(invoiceId string) (*InvoiceCheckResponse, error)
+	CheckTokenExpire() error
+}
+
+func New(baseURL string, username string, password string) Tino {
+	return &tino{
+		baserUrl: baseURL,
+		username: username,
+		password: password,
+	}
+}
+
+func (t *tino) CreateInvoice(invoice *InvoiceRequest) (*InvoiceResponse, error) {
+	err := t.CheckTokenExpire()
+	if err != nil {
+		return nil, err
+	}
+	client := resty.New()
+	defer client.Close()
+	var response *InvoiceResponse
+	res, err := client.R().
+		SetAuthToken(t.client.Token.Token).
+		SetBody(invoice).              // default request content type is JSON
+		SetResult(&InvoiceResponse{}). // or SetResult(LoginResponse{}).
+		SetError(&InvoiceResponse{}).  // or SetError(LoginError{}).
+		Post(t.baserUrl + "/merchant/invoice")
+	if err != nil {
+		return nil, err
+	}
+	if res.IsError() {
+		return nil, errors.New(res.Error().(string))
+	}
+	response = res.Result().(*InvoiceResponse)
+	return response, nil
+}
+
+func (t *tino) CancelInvoice(invoiceId string) (bool, error) {
+	err := t.CheckTokenExpire()
+	if err != nil {
+		return false, err
+	}
+	client := resty.New()
+	defer client.Close()
+	var response *InvoiceResponse
+	res, err := client.R().
+		SetAuthToken(t.client.Token.Token).
+		SetQueryParam("reason", "canceled").
+		SetResult(&InvoiceResponse{}). // or SetResult(LoginResponse{}).
+		SetError(&InvoiceResponse{}).  // or SetError(LoginError{}).
+		Post(t.baserUrl + "/merchant/invoice/cancel/" + invoiceId)
+	if err != nil {
+		return false, err
+	}
+	if res.IsError() {
+		return false, errors.New(res.Error().(string))
+	}
+	response = res.Result().(*InvoiceResponse)
+	if response.Data.Status != "cancelled" {
+		return false, errors.New("invoice not cancelled")
+	}
+	return true, nil
+}
+
+func (t *tino) CheckInvoice(invoiceId string) (*InvoiceCheckResponse, error) {
+	err := t.CheckTokenExpire()
+	if err != nil {
+		return nil, err
+	}
+	client := resty.New()
+	defer client.Close()
+	var response *InvoiceCheckResponse
+	res, err := client.R().
+		SetAuthToken(t.client.Token.Token).
+		SetResult(&InvoiceCheckResponse{}). // or SetResult(LoginResponse{}).
+		SetError(&InvoiceCheckResponse{}).  // or SetError(LoginError{}).
+		Get(t.baserUrl + "/merchant/invoice/" + invoiceId)
+	if err != nil {
+		return nil, err
+	}
+	if res.IsError() {
+		return nil, errors.New(res.Error().(string))
+	}
+	response = res.Result().(*InvoiceCheckResponse)
+	return response, nil
+}
+
+func (t *tino) CheckTokenExpire() error {
+	if !t.client.Token.ExpiresAt.Before(time.Now()) {
+		client := resty.New()
+		defer client.Close()
+		res, err := client.R().
+			SetBody(AuthRequest{
+				Username: t.username,
+				Password: t.password,
+			}).
+			SetResult(&AuthResponse{}). // or SetResult(LoginResponse{}).
+			SetError(&AuthResponse{}).  // or SetError(LoginError{}).
+			Post(t.baserUrl + "/merchant/login")
+		if err != nil {
+			return err
+		}
+		if res.IsError() {
+			return errors.New(res.Error().(string))
+		}
+		response := res.Result().(*AuthResponse)
+		t.client = &response.Data
+	}
+	return nil
+}
