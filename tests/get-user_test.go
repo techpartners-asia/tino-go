506fa195e388c5e421f46822683a76af8ae2fed4
Strong B Production <batbold.z@tehcpartners.asia>
2026-03-19 15:18:59 +0800

    Update TestGetUser to use test credentials for user retrieval, ensuring consistency in testing environment.

--- tests/get-user_test.go
+++ tests/get-user_test.go
@@ -8,8 +8,8 @@ import (
 )
 
 func TestGetUser(t *testing.T) {
-	tino := tino.New("https://auth-sandbox.tino.mn/api/v1", "https://payment-sandbox.tino.mn/api/v1", "ZAHII", "lGDixnEKUPvGHnl")
-	user, err := tino.GetUser("5545ff7dfc37c905061653a61c272f5f")
+	tino := tino.New("https://auth-sandbox.tino.mn/api/v1", "https://payment-sandbox.tino.mn/api/v1", "test", "test")
+	user, err := tino.GetUser("test")
 	if err != nil {
 		t.Errorf("Error getting user: %v", err)
 	}
